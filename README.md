9152f00e39eee10f0a8eacd2a69cb412d7a4cd14
FzhangSpace <312024054@qq.com>
2016-02-01 16:10:01 +0800

    wait time

--- README.md
+++ README.md
@@ -2,3 +2,117 @@
 simple interpreter of C. Inspired by c4 and largely based on it.
 
 source:http://blog.jobbole.com/97332/
+
+
+##step02:
+source:http://blog.jobbole.com/97359/
+
+###寄存器
+	
+	PC 程序计数器，它存放的是一个内存地址，该地址中存放着 下一条 要执行的计算机指令。
+	SP 指针寄存器，永远指向当前的栈顶。注意的是由于栈是位于高地址并向低地址增长的，所以入栈时 SP 的值减小。
+	BP 基址指针。也是用于指向栈的某些位置，在调用函数时会使用到它。
+	AX 通用寄存器，我们的虚拟机中，它用于存放一条指令执行后的结果。
+	
+	
+###指令集	
+	
+```c
+enum { LEA ,IMM ,JMP ,CALL,JZ  ,JNZ ,ENT ,ADJ ,LEV ,LI  ,LC  ,SI  ,SC  ,PUSH,
+       OR  ,XOR ,AND ,EQ  ,NE  ,LT  ,GT  ,LE  ,GE  ,SHL ,SHR ,ADD ,SUB ,MUL ,DIV ,MOD ,
+       OPEN,READ,CLOS,PRTF,MALC,MSET,MCMP,EXIT };
+``` 
+
+####MOV
+	
+	将数据放进寄存器或内存地址,类似C的赋值
+	用法  MOV source dest
+	表示将source的内容放在dest中,它们可以使一个数,寄存器,或是一个内存地址
+	
+	因为我们的虚拟机只有一个寄存器,再加上识别参数的类型是比较困难,
+	为了简化操作,将MOV指令分解成5个指令
+		- IMM <num>  将<num>放入寄存器ax中
+		- LC 	将对应地址中的字符载入ax中,要求ax中存放地址
+		- LI	将对应地址中的整数载入ax中,要求ax中存放地址
+		- SC	将ax中的数据作为字符存放入地址中,要求栈顶存放地址.
+		- SI  	将ax中的数据作为整数存放入地址中,要求栈顶存放地址
+
+####PUSH
+
+	将值或寄存器,而在我们的虚拟机中,它的作用是将ax的值放入栈中.
+	主要是为了家伙虚拟机的实现,并且我们也只有一个寄存器ax
+
+####JMP
+	
+	JMP <addr> 是跳转指令,无条件地将当前的PC寄存器设置为指定的<addr>
+
+####JZ/JNZ
+	
+	为了实现if语句,我们需要条件判断相关的指令
+	我们这里只是先两个简单的判断,即结果(ax)为零或为零情况下的跳转
+
+####子函数调用 CALL/ENT/ADJ/LEV
+	
+	首先介绍CALL <addr>和RET指令,CALL的作用是跳转到地址为<addr>的子函数,RET则用于从子函数中返回
+	不过我们不直接使用RET指令,我们后面使用LEV指令来替代它
+	
+	在实际调用函数时，不仅要考虑函数的地址，还要考虑如何传递参数和如何返回结果。
+	这里我们约定，如果子函数有返回结果，那么就在返回时保存在 ax 中，它可以是一个值，也可以是一个地址
+
+####ENT
+	
+	ENT <size> 值得是enter,用于实现"make new call frame"的功能
+	就是保存当前指针,同时在栈上保留一定空间,用以存放局部变量.
+	
+####ADJ
+	
+	ADJ <size> 用于实现 "remove arguments from frame"
+	在将调用子函数时,压入栈中的数据清除,本质上是因为我们的ADD指令功能有限
+	
+####LEV
+	
+	在执行完子函数之后,将重新调到之前调用子函数的位置
+	
+####LEA
+	
+	作用是为了在子函数中得到传入子函数的参数
+	
+####运算符指令集
+
+	OR  XOR  AND  EQ  NE  LT  LE  GT  GE  
+	SHL  SHR  ADD  SUB  MUL  DIV  MOD
+	
+####内置函数
+
+	编译器中我们需要用到的函数有：exit, open, close, read, printf, malloc, memset 及memcmp
+	
+###测试代码解析:
+	
+```c
+int main(int argc, char *argv[])
+{
+    ax = 0;
+    ...
+ 
+    i = 0;
+    text[i++] = IMM;
+    text[i++] = 10;
+    text[i++] = PUSH;
+    text[i++] = IMM;
+    text[i++] = 20;
+    text[i++] = ADD;
+    text[i++] = PUSH;
+    text[i++] = EXIT;
+    pc = text;
+ 
+    ...
+    program();
+}
+```
+
+
+
+
+
+
+
--- src/main.c
+++ src/main.c
@@ -3,10 +3,31 @@
 #include <memory.h>
 #include <string.h>
 
+/* 用于读取源文件内容,词法分析,语法分析 */
 int token;
 char *src, *old_src;
 int poolsize;
 int line;
+
+/* 模拟代码段/数据段以及栈 */
+int *text, *old_text, *stack;
+char *data;
+
+/* 虚拟计算寄存器 */
+int *pc, *bp, *sp, ax, cyle;
+/*
+	pc程序计数器 他存放的是内存地址,该地址中存放着下一条要执行的计算器指令
+	sp指针寄存器 永远指向当前的栈顶. 
+		注意的是由于栈是位于高地质并向低地址增长的,所以入栈是sp的值减小
+	bp基址指针.  也是用于指向栈的某些位置,在调用函数是使用到它
+	ax通用寄存器 在我们的虚拟机中,它用于存放一条指令执行后的结果
+*/
+
+/* CPU指令集 */
+enum { LEA ,IMM ,JMP ,CALL,JZ  ,JNZ ,ENT ,ADJ ,LEV ,LI  ,LC  ,SI  ,SC  ,PUSH,
+       OR  ,XOR ,AND ,EQ  ,NE  ,LT  ,GT  ,LE  ,GE  ,SHL ,SHR ,ADD ,SUB ,MUL ,DIV ,MOD ,
+       OPEN,READ,CLOS,PRTF,MALC,MSET,MCMP,EXIT };
+
 
 /* 用于词法分析，获取下一个标记，它将自动忽略空白字符 */
 void next() 
@@ -34,7 +55,65 @@ void program()
 /* 虚拟机的入口,用于解释目标代码 */
 int eval() 
 {
-    //do nothing yet
+    int op, *tmp;
+    while (1) {
+    	switch (op)
+    	{
+		case IMM:	ax = *pc++; break;
+		case LC:	ax = *(char*)ax; break;
+		case LI:	ax = *(int*)ax; break;
+		case SC:	ax = *(char *)*sp++ = ax; break;
+		case SI:	*(int*)*sp++ = ax; break;
+			
+		case PUSH:	*--sp = ax; break;
+		case JMP:	pc = (int*)*pc; break;
+		
+		case JZ:	pc = ax ? pc + 1 : (int*)*pc;	break;
+		case JNZ:	pc = ax ? (int*)*pc : pc + 1; break;
+			
+		case CALL:	*--sp = (int)(pc+1); pc = (int*)*pc; break;
+		case ENT:	*--sp = (int)bp; bp = sp; 
+					sp = sp - *pc++; break;
+		case ADJ:	sp = sp + *pc++; break;
+		case LEV:	sp = bp; bp = (int*)*sp++; 
+					pc = (int*)*sp++; break;
+		
+		case OR:	ax = *sp++ | ax; break;
+		case XOR:	ax = *sp++ ^ ax; break;
+		case AND: 	ax = *sp++ & ax; break;
+		case EQ:	ax = *sp++ == ax; break;
+		case NE:	ax = *sp++ != ax; break;
+		case LT:	ax = *sp++ < ax; break;
+		case LE:	ax = *sp++ <= ax; break;
+		case GT:	ax = *sp++ > ax; break;
+		case GE:	ax = *sp++ >= ax; break;
+		case SHL:	ax = *sp++ << ax; break;
+		case SHR:	ax = *sp++ >> ax; break;
+		case ADD:	ax = *sp++ + ax; break;
+		case SUB:	ax = *sp++ - ax; break;
+		case MUL:	ax = *sp++ * ax; break;
+		case DIV:	ax = *sp++ / ax; break;
+		case MOD:	ax = *sp++ % ax; break;
+			
+		case EXIT:	printf("exit(%d)", *sp);	return *sp;
+		case OPEN:	ax = open((char*)sp[1],sp[0]); break;
+		case CLOS:	ax = close(*sp); break;
+		case READ:	ax = read(sp[2], (char*)sp[1], *sp); break;
+		case PRTF:	tmp = sp + pc[1];
+					ax = printf((char*)tmp[-1], tmp[-2], tmp[-3], tmp[-4], tmp[-5], tmp[-6]);
+					break;
+		case MALC:	ax = (int)malloc(*sp); break;
+		case MSET:	ax = (int)memset((char*)sp[2], sp[1], *sp); break;
+		case MCMP:	ax = memcmp((char*)sp[2], (char*)sp[1], *sp); break;
+		default:
+			printf("unknown instruction:%d\n", op);
+			return -1;
+    	}
+    }
+    
+
+    
+    
     return 0;
 }
 
@@ -53,19 +132,51 @@ int main(int argc, char **argv)
         printf("could not open(%s)\n", *argv);
         return -1;
     }
-
     if (!(src = old_src = malloc(poolsize))) {
         printf("could not malloc(%d) for source area\n", poolsize);
         return -1;
     }
-
     if ((i = read(fd, src, poolsize-1)) < 0) {
         printf("read() returned %d\n", i);
         return -1;
     }
-
     src[i] = 0;
     close(fd);
+
+    /* allcate memory for virtual machine */
+    if (!(text = old_text = malloc(poolsize))) {
+        printf("could not malloc(%d) for text area\n", poolsize);
+        return -1;
+    }
+    if (!(data = malloc(poolsize))) {
+        printf("could not malloc(%d) for data area\n", poolsize);
+        return -1;
+    }
+    if (!(stack = malloc(poolsize))) {
+        printf("could not malloc(%d) for stack area\n", poolsize);
+        return -1;
+    }
+    memset(text, 0, poolsize);
+    memset(data, 0, poolsize);
+    memset(stack, 0, poolsize);
+    
+    /* 这是为了栈的增长方向是向下的,所以先把指针指向高地址 */
+    bp = sp = (int*)((int)stack + poolsize);
+    ax = 0;
+  
+    
+    
+    i = 0;
+    text[i++] = IMM;
+    text[i++] = 10;
+    text[i++] = PUSH;
+    text[i++] = IMM;
+    text[i++] = 20;
+    text[i++] = ADD;
+    text[i++] = PUSH;
+    text[i++] = EXIT;
+    pc = text;
+    
 
     /* 分析源码 */
     program();
